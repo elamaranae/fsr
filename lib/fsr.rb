e8c5da1a22b3751e27bd6cf58951ac95e6391fa8
Elamaran A E <maran@maran.dev>
2024-06-08 00:43:33 +0530

    fsr: initial commit

---
+++ lib/fsr.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require_relative 'fsr/version'
+
+# Run RSpec fast by avoiding full app boot
+#
+# ```rb
+#   listener = Runner.listen(
+#     [
+#       'spec/search/service/fql/builder_spec.rb:42'
+#     ],
+#     load:
+#       [
+#         'lib/search/service/fql/builder.rb',
+#         'lib/search/service/fql.rb',
+#         'lib/rts/util.rb'
+#       ]
+#   )
+#
+#   listener.start
+#   listener.stop
+# ```
+class Fsr
+  def self.listen(
+    run,
+    load: [],
+    listen:
+      [
+        "#{`pwd`.strip}/app",
+        "#{`pwd`.strip}/lib",
+        "#{`pwd`.strip}/spec"
+      ]
+  )
+    Listen.to(*listen) { new(run, load: load).run }
+  end
+
+  def initialize(specs, load: [])
+    @specs = specs
+    @dependent_files = load
+  end
+
+  def run
+    @dependent_files.each { |file| load(file) }
+    RSpec::Core::Runner.run(@specs)
+    cleanup
+  end
+
+  def cleanup
+    warn_level = $VERBOSE
+    $VERBOSE = nil
+    remove_rspec
+    require('rspec')
+    configure
+    $VERBOSE = warn_level
+  end
+
+  def remove_rspec
+    Object.send(:remove_const, 'RSpec')
+    $LOADED_FEATURES.reject! { |a| a.include?('rspec') }
+  end
+
+  def configure
+    load('spec/spec_helper.rb')
+    load('spec/rails_helper.rb')
+  end
+end
